3aae24dc25082c56e6ab4fac55a7434a35fecbb4
Grigorii Sharapov <socaseinpoint@gmail.com>
2019-10-24 16:40:54 +0600

    work in progress

--- src/components/partials/profile/board/filter/Filter.js
+++ src/components/partials/profile/board/filter/Filter.js
@@ -1,42 +1,55 @@
 import React from 'react';
+import { Form, Field } from 'react-final-form';
+import { InputGroup, SelectInput, Label } from '../../../../base/form';
+
+function onSubmit(e) {
+  console.log(e);
+}
 
 const ProfileFilter = () => (
   <div className="boss-board__manager-filter">
-    <form action="#" className="boss-form">
-      <div className="boss-form__group boss-form__group_position_last">
-        <h3 className="boss-form__group-title">Filter</h3>
-        <div className="boss-form__row boss-form__row_align_center boss-form__row_desktop boss-form__row_position_last">
-          <div className="boss-form__field boss-form__field_layout_quarter">
-            <label className="boss-form__label"><span className="boss-form__label-text">Type</span></label>
-            <div className="boss-form__select">
-              React Select
-              </div>
-            <div className="boss-form__field boss-form__field_layout_max boss-form__field_no-label">
-              <div className="date-range-picker date-range-picker_type_interval-fluid date-range-picker_type_icon date-range-picker_no-label">
-                React dates
-                <div className="boss-form__field boss-form__field_layout_min boss-form__field_no-label">
-                  <button className="boss-button boss-form__submit" type="submit">Update</button>
+    <Form
+      onSubmit={onSubmit}
+      render={({handleSubmit}) => (
+        <form action="#" className="boss-form">
+          <div className="boss-form__group boss-form__group_position_last">
+            <h3 className="boss-form__group-title">Filter</h3>
+            <div className="boss-form__row boss-form__row_align_center boss-form__row_desktop boss-form__row_position_last">
+              <div className="boss-form__field boss-form__field_layout_quarter">
+                <div className="boss-form__select">
+                  <InputGroup>
+                    <Label>Type</Label>
+                    <Field name="date" component={SelectInput} />
+                  </InputGroup>
                 </div>
-              </div>
-
-              <div className="boss-form__row boss-form__row_mobile boss-form__row_position_last">
-                <div className="boss-form__field boss-form__field_layout_quarter">
-                  <label className="boss-form__label"><span className="boss-form__label-text">Type</span></label>
-                  <div className="boss-form__select">
-                    React Select
+                <div className="boss-form__field boss-form__field_layout_max boss-form__field_no-label">
+                  <div className="date-range-picker date-range-picker_type_interval-fluid date-range-picker_type_icon date-range-picker_no-label">
+                    React dates
+                    <div className="boss-form__field boss-form__field_layout_min boss-form__field_no-label">
+                      <button className="boss-button boss-form__submit" type="submit">Update</button>
+                    </div>
                   </div>
-                  <div className="boss-form__field boss-form__field_layout_max">
-                    <div className="date-range-picker date-range-picker_type_interval-fluid date-range-picker_type_icon date-range-picker_no-label">
-                      React Dates
+
+                  <div className="boss-form__row boss-form__row_mobile boss-form__row_position_last">
+                    <div className="boss-form__field boss-form__field_layout_quarter">
+                      <label className="boss-form__label"><span className="boss-form__label-text">Type</span></label>
+                      <div className="boss-form__select">
+                        React Select
+                      </div>
+                      <div className="boss-form__field boss-form__field_layout_max">
+                        <div className="date-range-picker date-range-picker_type_interval-fluid date-range-picker_type_icon date-range-picker_no-label">
+                          React Dates
+                        </div>
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-      </div>
-    </form>
+        </form>
+      )}
+    />
   </div>
 );
 
