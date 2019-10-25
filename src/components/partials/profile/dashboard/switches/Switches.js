66088922ac99a734047700a8dc65a7ae5330d38b
Grigorii Sharapov <socaseinpoint@gmail.com>
2019-10-25 15:28:45 +0600

    Add StaffMember dashboard icons

--- src/components/partials/profile/dashboard/switches/Switches.js
+++ src/components/partials/profile/dashboard/switches/Switches.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import classNames from 'classnames';
 import PropTypes from 'prop-types';
 import { NavLink, useParams } from 'react-router-dom';
 
-const SwitchesLink = ({ to, label }) => (
-  <NavLink to={to} className="boss-button boss-button_type_small boss-button_role_profile boss-page-dashboard__switch" activeClassName="boss-button_state_active">
-    {label}
-  </NavLink>
-);
+const SwitchesLink = ({ to, label, htmlRole, }) => {
+  const classList = classNames('boss-button', 'boss-button_type_small', 'boss-page-dashboard__switch', { [`boss-button_role_${htmlRole}`]: htmlRole });
+  return (
+    <NavLink to={to} className={classList} activeClassName="boss-button_state_active">
+      {label}
+    </NavLink>
+  );
+};
 
 SwitchesLink.propTypes = {
   to: PropTypes.string.isRequired,
@@ -18,12 +22,12 @@ const Switches = () => {
 
   return (
     <div className="boss-page-dashboard__switches">
-      <SwitchesLink to={`/staff-member-profile/${id}/profile`} label="Profile" />
-      <SwitchesLink to={`/staff-member-profile/${id}/holidays`} label="Holidays" />
-      <SwitchesLink to={`/staff-member-profile/${id}/3`} label="Owed hours" />
-      <SwitchesLink to={`/staff-member-profile/${id}/4`} label="Accessories" />
-      <SwitchesLink to={`/staff-member-profile/${id}/5`} label="Shifts" />
-      <SwitchesLink to={`/staff-member-profile/${id}/6`} label="Payments" />
+      <SwitchesLink to={`/staff-member-profile/${id}/profile`} label="Profile" htmlRole="profile" />
+      <SwitchesLink to={`/staff-member-profile/${id}/holidays`} label="Holidays" htmlRole="holidays" />
+      <SwitchesLink to={`/staff-member-profile/${id}/3`} label="Owed hours" htmlRole="timelog" />
+      <SwitchesLink to={`/staff-member-profile/${id}/4`} label="Accessories" htmlRole="accessories" />
+      <SwitchesLink to={`/staff-member-profile/${id}/5`} label="Shifts" htmlRole="shifts" />
+      <SwitchesLink to={`/staff-member-profile/${id}/6`} label="Payments" htmlRole="payments" />
     </div>
   );
 };
