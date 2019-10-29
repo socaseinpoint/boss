d2202af1aa52e00781ec4668900a1f152606cfd0
Grigorii Sharapov <socaseinpoint@gmail.com>
2019-10-29 13:50:15 +0600

    Clean Up StaffMemberProfileEdit Component

--- src/components/pages/staff-member-profile/edit/Edit.js
+++ src/components/pages/staff-member-profile/edit/Edit.js
@@ -19,93 +19,115 @@ import ContactDetailsForm from '../../../partials/profile/edit/contact-details';
 const StaffMemberProfileEdit = ({ id }) => {
   const { path } = useRouteMatch();
   const initialValues = useSelector((state) => getEditFormInitialValues(state));
-  const { staffMember } = initialValues;
+  const {
+    staffMember: {
+      startsAt,
+      dayPreferenceNote,
+      hoursPreferenceNote,
+      nationalInsuranceNumber,
+      sageId,
+      firstName,
+      surname,
+      dateOfBirth,
+      email,
+      phoneNumber,
+      address,
+      postcode,
+      country,
+    },
+    venuesOptions,
+    staffTypesOptions,
+    payRatesOptions,
+    mainVenueInitial,
+    staffTypeInitial,
+    payRateInitial,
+    otherVenueInitial,
+    genderInitial,
+  } = initialValues;
 
   return (
-    staffMember ? (
-      <>
-        <Dashboard>
-          <div className="boss-page-dashboard boss-page-dashboard_updated boss-page-dashboard_page_profile-edit">
-            <div className="boss-page-dashboard__group">
-              <h1 className="boss-page-dashboard__title">Edit Profile</h1>
-              <div className="boss-page-dashboard__buttons-group boss-page-dashboard__buttons-group_position_last">
-                <Link to={`/staff-member-profile/${id}/profile`} className="boss-button boss-button_role_cancel boss-page-dashboard__button">Cancel Editing</Link>
-              </div>
+    <>
+      <Dashboard>
+        <div className="boss-page-dashboard boss-page-dashboard_updated boss-page-dashboard_page_profile-edit">
+          <div className="boss-page-dashboard__group">
+            <h1 className="boss-page-dashboard__title">Edit Profile</h1>
+            <div className="boss-page-dashboard__buttons-group boss-page-dashboard__buttons-group_position_last">
+              <Link to={`/staff-member-profile/${id}/profile`} className="boss-button boss-button_role_cancel boss-page-dashboard__button">Cancel Editing</Link>
             </div>
           </div>
-        </Dashboard>
-        <Content>
-          <Switcher
-            chapters={(
-              <Switch>
-                <Route
-                  exact
-                  path={`${path}/employment`}
-                >
-                  <Chapter header="Employment Details">
-                    <EmploymentDetailsForm
-                      venuesOptions={initialValues.venuesOptions}
-                      staffTypesOptions={initialValues.staffTypesOptions}
-                      startsAt={staffMember.startsAt}
-                      payRatesOptions={initialValues.payRatesOptions}
-                      dayPreferenceNote={staffMember.dayPreferenceNote}
-                      hoursPreferenceNote={staffMember.hoursPreferenceNote}
-                      nationalInsuranceNumber={staffMember.nationalInsuranceNumber}
-                      sageId={staffMember.sageId}
-                      mainVenueInitial={initialValues.mainVenueInitial}
-                      staffTypeInitial={initialValues.staffTypeInitial}
-                      payRateInitial={initialValues.payRateInitial}
-                      otherVenueInitial={initialValues.otherVenueInitial}
-                    />
-                  </Chapter>
-                </Route>
-                <Route
-                  exact
-                  path={`${path}/personal`}
-                >
-                  <Chapter header="Personal Details">
-                    <PersonalDetailsForm
-                      firstName={staffMember.firstName}
-                      surname={staffMember.surname}
-                      dateOfBirth={staffMember.dateOfBirth}
-                      genderInitial={initialValues.genderInitial}
-                    />
-                  </Chapter>
-                </Route>
-                <Route
-                  exact
-                  path={`${path}/contact`}
-                >
-                  <Chapter header="Contact Details">
-                    <ContactDetailsForm
-                      email={staffMember.email}
-                      phoneNumber={staffMember.phoneNumber}
-                      address={staffMember.address}
-                      postcode={staffMember.postcode}
-                      country={staffMember.country}
-                    />
-                  </Chapter>
-                </Route>
-              </Switch>
-            )}
-            nav={[
-              {
-                title: 'Employment details',
-                to: `/staff-member-profile/${id}/edit/employment`,
-              },
-              {
-                title: 'Personal details',
-                to: `/staff-member-profile/${id}/edit/personal`,
-              },
-              {
-                title: 'Contact details',
-                to: `/staff-member-profile/${id}/edit/contact`,
-              },
-            ]}
-          />
-        </Content>
-      </>
-    ) : null
+        </div>
+      </Dashboard>
+      <Content>
+        <Switcher
+          chapters={(
+            <Switch>
+              <Route
+                exact
+                path={`${path}/employment`}
+              >
+                <Chapter header="Employment Details">
+                  <EmploymentDetailsForm
+                    venuesOptions={venuesOptions}
+                    staffTypesOptions={staffTypesOptions}
+                    startsAt={startsAt}
+                    payRatesOptions={payRatesOptions}
+                    dayPreferenceNote={dayPreferenceNote}
+                    hoursPreferenceNote={hoursPreferenceNote}
+                    nationalInsuranceNumber={nationalInsuranceNumber}
+                    sageId={sageId}
+                    mainVenueInitial={mainVenueInitial}
+                    staffTypeInitial={staffTypeInitial}
+                    payRateInitial={payRateInitial}
+                    otherVenueInitial={otherVenueInitial}
+                  />
+                </Chapter>
+              </Route>
+              <Route
+                exact
+                path={`${path}/personal`}
+              >
+                <Chapter header="Personal Details">
+                  <PersonalDetailsForm
+                    firstName={firstName}
+                    surname={surname}
+                    dateOfBirth={dateOfBirth}
+                    genderInitial={genderInitial}
+                  />
+                </Chapter>
+              </Route>
+              <Route
+                exact
+                path={`${path}/contact`}
+              >
+                <Chapter header="Contact Details">
+                  <ContactDetailsForm
+                    email={email}
+                    phoneNumber={phoneNumber}
+                    address={address}
+                    postcode={postcode}
+                    country={country}
+                  />
+                </Chapter>
+              </Route>
+            </Switch>
+          )}
+          nav={[
+            {
+              title: 'Employment details',
+              to: `/staff-member-profile/${id}/edit/employment`,
+            },
+            {
+              title: 'Personal details',
+              to: `/staff-member-profile/${id}/edit/personal`,
+            },
+            {
+              title: 'Contact details',
+              to: `/staff-member-profile/${id}/edit/contact`,
+            },
+          ]}
+        />
+      </Content>
+    </>
   );
 };
 
