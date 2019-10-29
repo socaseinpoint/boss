f459ca796b4e3393d111bd6e62249574620cddd0
Grigorii Sharapov <socaseinpoint@gmail.com>
2019-10-29 13:40:53 +0600

    Refactor pagination

--- src/components/partials/pagination/Pagination.js
+++ src/components/partials/pagination/Pagination.js
@@ -6,91 +6,78 @@ const Pagination = ({
   total,
   handleAction,
   perPage,
-}) => (
-  <>
-    <div className="boss-page-main__count boss-page-main__count_space_large">
-      <span className="boss-page-main__count-text">Showing</span>
-      {' '}
-      <span className="boss-page-main__count-text boss-page-main__count-text_marked">{perPage}</span>
-      {' '}
-      <span className="boss-page-main__count-text">of</span>
-      {' '}
-      <span className="boss-page-main__count-text boss-page-main__count-text_marked">{total}</span>
-    </div>
-
-    <div className="boss-paginator boss-paginator_position_last">
-      <span
-        role="presentation"
-        onClick={() => handleAction(current - 1)}
-        className="boss-paginator__action boss-paginator__action_role_prev boss-paginator__action_state_disabled"
-      >
-      Previous
-      </span>
-
+}) => {
+  function renderPaginator(incrementor) {
+    return (
       <span
         role="presentation"
         onClick={() => handleAction(current)}
-        className="boss-paginator__action boss-paginator__action_role_current boss-paginator__action_state_active"
+        className="boss-paginator__action boss-paginator__action_role_current"
       >
-        {current}
+        {incrementor}
       </span>
+    );
+  }
 
-      <span
-        role="presentation"
-        onClick={() => handleAction(current + 1)}
-        className="boss-paginator__action"
-      >
-        {current + 1}
-      </span>
+  return (
+    <>
+      <div className="boss-page-main__count boss-page-main__count_space_large">
+        <span className="boss-page-main__count-text">Showing</span>
+        {' '}
+        <span className="boss-page-main__count-text boss-page-main__count-text_marked">{perPage}</span>
+        {' '}
+        <span className="boss-page-main__count-text">of</span>
+        {' '}
+        <span className="boss-page-main__count-text boss-page-main__count-text_marked">{total}</span>
+      </div>
 
-      <span
-        role="presentation"
-        onClick={() => handleAction(current + 2)}
-        className="boss-paginator__action"
-      >
-        {current + 2}
-      </span>
+      <div className="boss-paginator boss-paginator_position_last">
+        <span
+          role="presentation"
+          onClick={() => handleAction(current - 1)}
+          className="boss-paginator__action boss-paginator__action_role_prev boss-paginator__action_state_disabled"
+        >
+        Previous
+        </span>
 
-      <span
-        role="presentation"
-        onClick={() => handleAction(current + 3)}
-        className="boss-paginator__action"
-      >
-        {current + 3}
-      </span>
+        <span
+          role="presentation"
+          onClick={() => handleAction(current)}
+          className="boss-paginator__action boss-paginator__action_role_current boss-paginator__action_state_active"
+        >
+          {current}
+        </span>
 
-      <span
-        role="presentation"
-        onClick={() => handleAction(current + 4)}
-        className="boss-paginator__action"
-      >
-        {current + 4}
-      </span>
+        {renderPaginator(current + 1)}
+        {renderPaginator(current + 2)}
+        {renderPaginator(current + 3)}
+        {renderPaginator(current + 4)}
 
-      <span
-        role="presentation"
-        className="boss-paginator__action boss-paginator__action_role_delimiter"
-      >
-      ...
-      </span>
+        <span
+          role="presentation"
+          className="boss-paginator__action boss-paginator__action_role_delimiter"
+        >
+        ...
+        </span>
 
-      <span
-        role="presentation"
-        onClick={() => handleAction(total)}
-        className="boss-paginator__action boss-paginator__action_role_total"
-      >
-        {total}
-      </span>
-      <span
-        role="presentation"
-        onClick={() => handleAction(current + 1)}
-        className="boss-paginator__action boss-paginator__action_role_next"
-      >
-      Next
-      </span>
-    </div>
-  </>
-);
+        <span
+          role="presentation"
+          onClick={() => handleAction(total)}
+          className="boss-paginator__action boss-paginator__action_role_total"
+        >
+          {total}
+        </span>
+        <span
+          role="presentation"
+          onClick={() => handleAction(current + 1)}
+          className="boss-paginator__action boss-paginator__action_role_next"
+        >
+          Next
+        </span>
+      </div>
+    </>
+  );
+};
 
 Pagination.propTypes = {
   current: PropTypes.number.isRequired,
